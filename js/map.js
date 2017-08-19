20cefdd15a1c8946b9420ab57d56cb9790d7126a
halvomez <northdell@gmail.com>
2017-08-19 21:46:53 +0300

    структура объявления и рандом

--- js/map.js
+++ js/map.js
@@ -1,20 +1,32 @@
 'use strict';
 
-var adverts = [8];
-var advertsRandom = {
-  'avatar': '',
-  'title': '',
-  'address': '{{location.x}}, {{location.y}}',
-  'price': calcRandom(1000000, 1000),
-  'type': ['flat', 'house', 'bungalo'],
-  'rooms': calcRandom(5, 1),
-  'guests': calcRandom(10, 1),
-  'checkin': ['12:00', '13:00', '14:00'],
-  'checkout': ['12:00', '13:00', '14:00'],
-  'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  'description': '',
-  'photos': []
-};
+var arrAdverts = [8];
+
+function randomAdverts() {
+  var adverts = {
+    'author': {
+      'avatar': []
+    },
+    'location': {
+      'x': calcRandom(900, 100),
+      'y': calcRandom(500, 100)
+    },
+    'offer': {
+      'title': '',
+      'address': '{{location.x}}, {{location.y}}',
+      'price': calcRandom(1000000, 1000),
+      'type': ['flat', 'house', 'bungalo'],
+      'rooms': calcRandom(5, 1),
+      'guests': calcRandom(10, 1),
+      'checkin': ['12:00', '13:00', '14:00'],
+      'checkout': ['12:00', '13:00', '14:00'],
+      'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+      'description': '',
+      'photos': []
+    }
+  };
+  return adverts;
+}
 
 function calcRandom(max, min) {
   var random = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,7 +34,7 @@ function calcRandom(max, min) {
 }
 
 for (var i = 0; i < 8; i++) {
-  adverts[i] = advertsRandom;
+  arrAdverts[i] = randomAdverts();
 
 }
-console.log(adverts);
+console.log(arrAdverts[1]);
