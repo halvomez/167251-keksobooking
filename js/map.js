a13c4b14c7cc556e06f50cdd9484cf5e70848793
halvomez <northdell@gmail.com>
2017-08-19 20:56:29 +0300

    массив с объявлениями (одинаковыми)

--- js/map.js
+++ js/map.js
@@ -1,11 +1,28 @@
 'use strict';
 
-var adverts = {
-  'avatar': randomAvatar()
+var adverts = [8];
+var advertsRandom = {
+  'avatar': '',
+  'title': '',
+  'address': '{{location.x}}, {{location.y}}',
+  'price': calcRandom(1000000, 1000),
+  'type': ['flat', 'house', 'bungalo'],
+  'rooms': calcRandom(5, 1),
+  'guests': calcRandom(10, 1),
+  'checkin': ['12:00', '13:00', '14:00'],
+  'checkout': ['12:00', '13:00', '14:00'],
+  'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  'description': '',
+  'photos': []
+};
+
+function calcRandom(max, min) {
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return random;
 }
 
-function randomAvatar() {
-  var avatar = 'img/avatars/user0' + Math.floor(Math.random() * 8 + 1) + '.png';
-  return avatar;
+for (var i = 0; i < 8; i++) {
+  adverts[i] = advertsRandom;
+
 }
-console.log(adverts.avatar);
+console.log(adverts);
