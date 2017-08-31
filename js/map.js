f003e4896ea6b9bba8122caf5c0c75d55d43a3a8
halvomez <northdell@gmail.com>
2017-08-31 13:10:27 +0300

    pin.js IIEF

--- js/map.js
+++ js/map.js
@@ -3,23 +3,6 @@
 var ENTER_CODE = 13;
 var ESC_CODE = 27;
 
-var pinMapWidth = 56;
-var pinMapHeight = 75;
-
-var pinMap = document.querySelector('.tokyo__pin-map');
-var fragment = document.createDocumentFragment();
-
-for (var i = 0; i < adverts.length; i++) {
-  var randomPin = document.createElement('div');
-  randomPin.innerHTML = '<div class = "pin pin--create pin--' + i + '" style = "left: ' +
-    (adverts[i].location.x - pinMapWidth / 2) + 'px;' +
-      ' top: ' + (adverts[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
-      '<img src="' + adverts[i].author.avatar + '" class = "rounded" width="40" height="40">' +
-      '</div>';
-  fragment.appendChild(randomPin);
-}
-pinMap.appendChild(fragment);
-
 var lodgeTemplate = document.querySelector('#lodge-template').content.querySelector('.dialog__panel');
 
 function showAdwert(numberOfAdvert) {
---
+++ js/pin.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function drawPins() {
+
+  var pinMapWidth = 56;
+  var pinMapHeight = 75;
+
+  var pinMap = document.querySelector('.tokyo__pin-map');
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < adverts.length; i++) {
+    var randomPin = document.createElement('div');
+    randomPin.innerHTML = '<div class = "pin pin--create pin--' + i + '" style = "left: ' +
+    (adverts[i].location.x - pinMapWidth / 2) + 'px;' +
+    ' top: ' + (adverts[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
+    '<img src="' + adverts[i].author.avatar + '" class = "rounded" width="40" height="40">' +
+    '</div>';
+    fragment.appendChild(randomPin);
+  }
+  pinMap.appendChild(fragment);
+})();
