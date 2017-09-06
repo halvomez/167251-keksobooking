2ca06f5d1d2e57d92f1f588171a6c012488b9fbc
Aleksei Kharitonov <northdell@gmail.com>
2017-09-06 11:40:34 +0300

    добавление Pins после загрузки данных

--- js/pin.js
+++ js/pin.js
@@ -4,20 +4,27 @@
   var pinMapWidth = 56;
   var pinMapHeight = 75;
 
-  window.loadBackend(function (serverAdverts) {
+  window.backend.load(getData);
 
+  function getData(serverData) {
+    if (typeof serverData === 'object') {
+      addPins(serverData);
+    }
+  }
+
+  function addPins(serverData) {
     var pinMap = document.querySelector('.tokyo__pin-map');
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < serverAdverts.length; i++) {
+    for (var i = 0; i < serverData.length; i++) {
       var randomPin = document.createElement('div');
       randomPin.innerHTML = '<div class = "pin pin--create pin--' + i + '" style = "left: ' +
-    (serverAdverts[i].location.x - pinMapWidth / 2) + 'px;' +
-    ' top: ' + (serverAdverts[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
-    '<img src="' + serverAdverts[i].author.avatar + '" class = "rounded" width="40" height="40">' +
-    '</div>';
+      (serverData[i].location.x - pinMapWidth / 2) + 'px;' +
+      ' top: ' + (serverData[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
+      '<img src="' + serverData[i].author.avatar + '" class = "rounded" width="40" height="40">' +
+      '</div>';
       fragment.appendChild(randomPin);
     }
     pinMap.appendChild(fragment);
-  });
+  }
 })();
