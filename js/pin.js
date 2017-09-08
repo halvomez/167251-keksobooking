bc8f6ec49413af85d7de0e46964a1a7046e5167e
Aleksei Kharitonov <northdell@gmail.com>
2017-09-08 15:00:53 +0300

    тест сортировки массива данных с сервера

--- js/pin.js
+++ js/pin.js
@@ -3,26 +3,44 @@
 (function drawPins() {
   var pinMapWidth = 56;
   var pinMapHeight = 75;
+  var pins = [];
 
   window.backend.load(getData);
 
   function getData(serverData) {
     if (typeof serverData === 'object') {
-      addPins(serverData);
+      pins = serverData;
+      addPins(pins);
       window.activatePin();
     }
   }
 
-  function addPins(serverData) {
+
+  function renderPins() {
+    var filtredPins = pins.filter(function (pin) {
+      return pin.offer.type === 'flat';
+    });
+    console.log(filtredPins);
+  }
+
+  var formFilter = document.forms[0];
+  var houseType = formFilter.querySelector('#housing_type');
+  var housePrice = formFilter.querySelector('#housing_price');
+  var housingRoomNumber = formFilter.querySelector('#housing_room-number');
+  var housingGuestsNumber = formFilter.querySelector('#housing_guests-number');
+
+  houseType.addEventListener('change', renderPins);
+
+  function addPins() {
     var pinMap = document.querySelector('.tokyo__pin-map');
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < serverData.length; i++) {
+    for (var i = 0; i < pins.length; i++) {
       var randomPin = document.createElement('div');
       randomPin.innerHTML = '<div class = "pin pin--create pin--' + i + '" style = "left: ' +
-      (serverData[i].location.x - pinMapWidth / 2) + 'px;' +
-      ' top: ' + (serverData[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
-      '<img src="' + serverData[i].author.avatar + '" class = "rounded" width="40" height="40">' +
+      (pins[i].location.x - pinMapWidth / 2) + 'px;' +
+      ' top: ' + (pins[i].location.y - pinMapHeight) + 'px"  tabindex="0">' +
+      '<img src="' + pins[i].author.avatar + '" class = "rounded" width="40" height="40">' +
       '</div>';
       fragment.appendChild(randomPin);
     }
