51a3726bcf9f8275437fde674b3c3dd954a47b23
Aleksei Kharitonov <northdell@gmail.com>
2017-09-14 13:42:38 +0300

    генерация пинов  через forEach pin.js

--- js/pin.js
+++ js/pin.js
@@ -9,17 +9,16 @@
   function addPins(data) {
 
     var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < data.length; i++) {
-      var randomPin = document.createElement('div');
-      randomPin.classList.add('div--create');
-      randomPin.innerHTML = '<div class = "pin pin--create pin--' + i + '" style = "left: ' +
-      (data[i].location.x - PIN_WIDTH / 2) + 'px;' +
-      ' top: ' + (data[i].location.y - PIN_HEIGHT) + 'px"  tabindex="0">' +
-      '<img src="' + data[i].author.avatar + '" class = "rounded" width="40" height="40">' +
+    data.forEach(function (pin, number) {
+      pin = document.createElement('div');
+      pin.classList.add('div--create');
+      pin.innerHTML = '<div class = "pin pin--create pin--' + number + '" style = "left: ' +
+      (data[number].location.x - PIN_WIDTH / 2) + 'px;' +
+      ' top: ' + (data[number].location.y - PIN_HEIGHT) + 'px"  tabindex="0">' +
+      '<img src="' + data[number].author.avatar + '" class = "rounded" width="40" height="40">' +
       '</div>';
-      fragment.appendChild(randomPin);
-    }
+      fragment.appendChild(pin);
+    });
     pinMap.appendChild(fragment);
   }
   window.addPins = addPins;
